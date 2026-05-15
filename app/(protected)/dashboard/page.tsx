cab4cb5b626e356e728244573c94c058ac78c137
Filip Cizkovsky <filous.doma@volny.cz>
2026-05-15 14:35:00 +0200

    check admin role logic

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,10 +1,14 @@
 type pageProps = {};
 import Link from 'next/link';
 
-export default function page({}: pageProps) {
+import hasAdminRole from '@/lib/hasAdminRole';
+
+export default async function page({}: pageProps) {
+    const isAdmin = await hasAdminRole();
+
     return (
         <>
-            <h1>dashboard</h1>
+            <h1>dashboard {isAdmin ? 'admin' : 'uzivatel'}</h1>
             <Link href="/logout" className="btn btn-lg sm:min-w-64">
                 logout
             </Link>
