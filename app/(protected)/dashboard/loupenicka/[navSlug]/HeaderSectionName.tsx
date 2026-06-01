5f8a60f10ae24064b31c262f8675c8b17387e08e
Filip Cizkovsky <filous.doma@volny.cz>
2026-06-01 14:45:53 +0200

    use name instead of slug for displayng loupenicko name

--- app/(protected)/dashboard/loupenicka/[navSlug]/HeaderSectionName.tsx
+++ app/(protected)/dashboard/loupenicka/[navSlug]/HeaderSectionName.tsx
@@ -1,8 +1,13 @@
+import { getLoupenickaBySlug } from '@/lib/db/nav';
+
 type HeaderSectionNameProps = {
     params: Promise<{ navSlug: string }>;
 };
 
 export default async function HeaderSectionName({ params }: HeaderSectionNameProps) {
     const { navSlug } = await params;
-    return <h2 className="mb-1">{navSlug}</h2>;
+
+    const { name } = await getLoupenickaBySlug(navSlug);
+
+    return <h2 className="mb-1">{name}</h2>;
 }
